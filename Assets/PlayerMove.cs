be66a1312bc1deed30301a9cf45d8bf7979ba9ae
e012052020alice <119144400+e012052020alice@users.noreply.github.com>
2023-04-15 14:44:52 +0800

    Initial commit

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{   public float speed=3.0f;
+	public bool moved=false;
+    void Update()
+    {
+        if(moved){
+			transform.position=transform.position+Camera.main.transform.forward*speed*Time.deltaTime;
+		}
+    }
+}
